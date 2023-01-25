6fc93b5bcbe097a46cc849e5f7f388af496f0150
LesterMartell <86186290+LesterMartell@users.noreply.github.com>
2023-01-25 15:59:44 +0000

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sad");
+
+            Console.Write("Укажите, площадь какой фигуры требуется вычислить:\n1. Треугольник\n2. Окружность\n\nВведите номер фигуры - ");
+
+            int choose; 
+            bool _try = Int32.TryParse(Console.ReadLine(), out choose);
+
+            if (_try && choose == 1)
+            {
+                Console.WriteLine(TriangleSquare(3,4,5));
+            }
+            else if (_try && choose == 2)
+            {
+                Console.WriteLine("\nОкружность.");
+            }
+            else
+            {
+                Console.WriteLine("\nВведено некорректное значение.");
+            }
+        }
+
+        static double TriangleSquare(double a, double b, double c)
+        {
+            // Площадь треугольника S = √(p·(p - a)·(p - b)·(p - c)), P = (a+b+c)/2
+            double p = (a+b+c)/2; // Полупериметр
+
+            double result = Math.Sqrt(p*(p-a)*(p-b)*(p-c));;
+
+            return result;
+        }
+
+        static int CircleSquare()
+        {
+            // Площадь круга S=πR²
+
+            return 0;
         }
     }
 }
